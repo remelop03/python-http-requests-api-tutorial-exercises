2e4b17b9b77463c408996e0ff17a1deb8db0f83a
remelop03 <remelop@unal.edu.co>
2025-05-24 18:09:45 +0000

    exercise_09

--- exercises/08-blog-post-author/app.py
+++ exercises/08-blog-post-author/app.py
@@ -1,3 +1,17 @@
 import requests
 
 # Your code here
+url = "https://assets.breatheco.de/apis/fake/sample/weird_portfolio.php"
+response = requests.get(url)
+data = response.json()
+
+# Getting the first post
+first_post = data["posts"][0]
+        
+# Getting the author dictionary
+author_dict = first_post["author"]
+        
+# Getting the author name
+author_name = author_dict["name"]
+
+print(author_name)
--- exercises/09-list-of-blog-titles/app.py
+++ exercises/09-list-of-blog-titles/app.py
@@ -1,8 +1,12 @@
 import requests
 
-def get_titles():
+def get_titles(web_page):
     # Your code here
-    return None
+    titles = []
+    response = requests.get(url)
+    data = response.json()
+    post = data["posts"]
+    return post
 
-
-print(get_titles())
+url = "https://assets.breatheco.de/apis/fake/sample/weird_portfolio.php"
+print(get_titles(url))
